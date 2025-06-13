57820ef6a916e1be2bb0991a89e8cc8985c62e5f
Husnucan <husnucan.demirci@hotmail.com>
2025-06-13 15:12:53 +0300

    a small fix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import CalculatorLogo from "@/app/components/CalculatorLogo";
 import CalculatorHeader from "@/app/components/CalculatorHeader";
 import CalculatorsGrid from "@/app/components/CalculatorsGrid";
-import { getCalculators } from "@/lib/calculators";
-import { Calculator } from "@/app/model/types";
+import { getCalculatorsAndCategories } from "@/lib/calculatorsAndCategories";
+import { Calculator, CalculatorAndCategoryData } from "@/app/model/types";
 
 export const revalidate = false;
 
-export default async function Page({}) {
-  const calculators: Calculator[] = await getCalculators();
+export default async function Page({ }) {
+  const calculatorAndCategoryData: CalculatorAndCategoryData = await getCalculatorsAndCategories();
   return (
     <main className="w-full px-4 md:px-8 py-6 md:py-3">
       <div className="mx-auto max-w-5xl h-auto md:min-h-full flex flex-col gap-5 pb-10">
         <CalculatorLogo></CalculatorLogo>
         <CalculatorHeader></CalculatorHeader>
-        <CalculatorsGrid calculators={calculators}></CalculatorsGrid>
+        <CalculatorsGrid calculators={calculatorAndCategoryData.calculators}></CalculatorsGrid>
       </div>
     </main>
   );
